448d26202fb6c57e436c101ed152aabc006fd9b6
williamimmanuelabadi <williamimmanuelabadi1@gmail.com>
2023-04-26 16:02:00 +0700

    search

--- course/search.php
+++ course/search.php
@@ -203,12 +203,122 @@
 					
 
 					<?php
-						for($i=0;$i<5;$i++){
-							include $_SERVER['DOCUMENT_ROOT'].'/dbmid/model/card/index.php';
-							include $_SERVER['DOCUMENT_ROOT'].'/dbmid/model/card/cardtest.php';
+						// Database connection details
+						$host = "localhost";
+						$username = "hj";
+						$password = "test1234";
+						$dbname = "dbmid";
+
+						// Connect to database
+						$conn = mysqli_connect($host, $username, $password, $dbname);
+
+						// Check connection
+						if (!$conn) {
+							die("Connection failed: " . mysqli_connect_error());
 						}
-						
-					?>
+
+						// Define variables for filters
+						$search_query = $department = $week = $time = "";
+
+						// Handle form submission
+						if ($_SERVER["REQUEST_METHOD"] == "POST") {
+							// Get filter values from form
+							$search_query = ($_POST["search_query"]);
+							$department =($_POST["department"]);
+							$week =($_POST["week"]);
+							$time =($_POST["time"]);
+							
+							if($department=="科系"){
+								$department="";
+							}
+							if($week=="星期"){
+								$week="";
+							}
+							if($time=="節"){
+								$time="";
+							}
+							
+							// Build SQL query based on filters
+							$sql = "SELECT * FROM course WHERE department LIKE '%$department%' AND week LIKE '%$week%' AND time LIKE '%$time%'";
+							if (!empty($search_query)) {
+								$sql .= " AND (course_name LIKE '%$search_query%' OR course_code LIKE '%$search_query%' OR teacher_name LIKE '%$search_query%')";
+							}
+							
+							// Execute query and get results
+							 $result = mysqli_query($conn, $sql);
+							 while ($row = mysqli_fetch_assoc($result)){
+							if (mysqli_num_rows($result) > 0) {
+								// Print results in a table?>
+								<div class="col-12 col-md-6 col-lg-4 col-xl-3 px-2">
+
+									<div class="col card mb-3 pt-2 px-2 pb-1" data-bs-toggle="modal" data-bs-target="#exampleModal">
+										
+										<div class="row pt-0 pb-0 px-3 mb-2 pt-1">
+											<div class="col-auto text-right px-0 m-0">           
+												<div class="h-100 d-flex align-items-end flex-column">
+													<h5 class="bi px-3 py-1 m-0">必</h5>
+												</div>
+											</div>
+											
+											<div class="col text-left pe-0 ps-1 m-0  d-flex align-items-center">
+												<h5 class="fw-bold ellipsis-1 m-0 ">
+													<span><?php echo $row['course_name']; ?></span>
+												</h5>
+											</div>          
+										</div>
+
+										<hr class="my-2">
+										
+										<div class="row pb-0 px-3 mb-2">
+											<div class="col text-left px-0 m-0">
+												<h5 class="fw-bold m-0"><?php echo $row['teacher_name']; ?></h5>  
+											</div>
+											<div class="col-auto text-right px-0 m-0">           
+												<div class="h-100 d-flex align-items-center">
+													<h5 class="fw-bold  m-0">代碼&nbsp;<span><?php echo $row['course_code']; ?></span></h5>
+												</div>
+											</div>
+										</div>
+
+										<div class="row pb-0 px-3 mb-2">
+											<div class="col text-left px-0 m-0">
+												<h5 class="m-0">人數&nbsp;<span>60</span>/<span>70</span></h5>
+											</div>
+											<div class="col-auto text-right px-0 m-0">           
+												<div class="h-100 d-flex align-items-center">
+													<h5 class="m-0">學分&nbsp;<span>2</span></h5>
+												</div>
+											</div>
+										</div>
+
+										<hr class="my-2">
+										
+										<div class="row pb-0 px-3 mb-2">
+											<div class="col text-left px-0 m-0">
+												<div class="h-100 d-flex align-items-center">
+													<h5 class="m-0"><?php echo $row['week'] . ' ' . $row['time']; ?></h5>
+												</div>
+											</div>
+											<div class="col-auto text-right px-0 m-0">           
+												<div class="h-100 d-flex align-items-center">
+													<h5 class="m-0">周五 1~4節</h5>
+												</div>
+											</div>
+										</div>   
+									</div>
+									
+								</div>
+							<?php
+							} else {
+								echo "No results found.";
+							}
+							}
+							
+						}
+
+						// Close database connection
+						mysqli_close($conn);
+						?>
 
 				</div>
 			</div>
