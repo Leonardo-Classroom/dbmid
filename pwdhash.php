914b5f5efdf11fd1e25d6e9835c600fd22bc0100
Leonardo-Classroom <leonardoclassroom@gmail.com>
2023-04-27 17:52:11 +0800

    密码制作

---
+++ pwdhash.php
@@ -0,0 +1,18 @@
+<?php
+
+for($i=1;$i<=500;$i++){
+    $password="D1176";
+    $temp=strval($i);
+    for($j=0;$j<3-strlen($temp);$j++){
+        $password.="0";
+    }
+    $password.=strval($i);
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    echo $hashed_password."<br>";
+
+    // echo $password."<br>";
+}
+
+
+?>
