0923541a8feba933d339b05863be9649ebf0b399
Johnny <131636923+JohnnyLin999@users.noreply.github.com>
2023-05-05 00:05:36 +0800

    根據學生的section_id匯入對應上課時間

--- model/timetable/index.php
+++ model/timetable/index.php
@@ -1,5 +1,143 @@
+
+
+<?php
+	// Database connection details
+	$host = "localhost";
+	$username = "hj";
+	$password = "test1234";
+	$dbname = "dbmid";
+
+	// Connect to database
+	$conn = mysqli_connect($host, $username, $password, $dbname);
+
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+
+	
+	// Build SQL query based on filters
+	$sql = "
+		SELECT * 
+		FROM `section_student`
+		WHERE `student_id` = 1
+	";
+
+	// Execute query and get results
+	$result = mysqli_query($conn, $sql);
+	
+	$section_student_id=[];
+	$section_id=[];
+	$student_id=[];
+	$is_withdrawable=[];
+	$is_valid=[];
+
+	while($row = mysqli_fetch_array($result)){
+
+        array_push($section_student_id, $row['section_student_id']);
+		array_push($section_id, $row['section_id']);
+		array_push($student_id, $row['student_id']);
+		array_push($is_withdrawable, $row['is_withdrawable']);
+		array_push($is_valid, $row['is_valid']);
+        	        
+	}
+
+
+	// Build SQL query based on filters
+	$result =[];
+	for($i=0;$i<count($section_id);$i++)
+	{
+		$arr[$section_id[$i]]["week"]=[];
+		$arr[$section_id[$i]]["time_start"]=[];
+		$arr[$section_id[$i]]["time_end"]=[];
+		
+		$sql = "
+		Select section_id, week, time_start, time_end
+		from `section_detail`
+		where section_id=".$section_id[$i]."
+		";
+
+		$temp = mysqli_query($conn, $sql);
+		// Execute query and get results
+
+		while($row = mysqli_fetch_array($temp)){
+			
+			
+			echo $row['week']." ";
+			echo $row['time_start']." ";
+			echo $row['time_end']." ";
+			echo "<br>";
+			//Store the data of same section_id in the same column(3D)
+			array_push($arr[$section_id[$i]]["week"],$row['week']);
+			array_push($arr[$section_id[$i]]["time_start"],$row['time_start']);
+			array_push($arr[$section_id[$i]]["time_end"],$row['time_end']);
+		}
+
+		// array_push(,mysqli_fetch_array($temp));
+	}
+	// $week=[];
+	// $time_start=[];
+	// $time_end=[];
+
+	// while($row = mysqli_fetch_array($result)){
+	// 	array_push($week, $row['week']);
+	// 	array_push($time_start, $row['time_start']);
+	// 	array_push($time_end, $row['time_end']); 
+	// }
+
+	for($i=0;$i<count($section_id);$i++){
+		for($j=0;$j<count($arr[$section_id[$i]]["week"]);$j++){
+			// echo $arr[$section_id[$i]]["week"][$j]." ";
+			// echo $arr[$section_id[$i]]["time_start"][$j]." ";
+			// echo $arr[$section_id[$i]]["time_end"][$j]."<br><br>";
+		}
+	}
+//Insert the section_id in the coresponding box
+
+
+
+
+	// continue
+	echo $account."<br>";
+	for($i=0;$i<count($section_id);$i++){
+
+		// echo $section_id[$i]."<br>";
+
+	}
+	
+	for($i=0;$i<13;$i++){
+		for($j=0;$j<5;$j++){
+			$table[$i][$j]=0;
+		}
+	}
+
+	for($i=0;$i<13;$i++){
+		for($j=0;$j<5;$j++){
+			echo $table[$i][$j]." ";
+		}echo "<br>";
+	}
+	
+
+
+
+	mysqli_close($conn);
+?>
+
+
+
+
+
+
+
+
 <link href="/dbmid/table.css" rel="stylesheet" type="text/css" />
 <div class="col-12 px-1" id="printArea">
+
+
+	
+		
+
+
 
 	<div class="col px-2">
 
@@ -52,7 +190,20 @@
 				<p class="m-0 p-0 form-ellipsis "></p>
 			</div>
 
-			<div class="col border f-height centerVertically px-1 bi-table" id="d4-1">
+			<div  
+			
+				<?php
+
+$isBi=0;
+
+					if($isBi){
+						echo 'class="col border f-height centerVertically px-1 bi-table"';
+					}else{
+						echo 'class="col border f-height centerVertically px-1 xuan-table"';
+					}				
+				?>
+			
+			>
 				
 				<p class="m-0 p-0 form-ellipsis " id="bi">資料庫管理系統</p>
 			</div>
@@ -77,7 +228,12 @@
 
 			<div class="col border f-height centerVertically px-1 bi-table" id="d2-2">
 				              
-				<p class="m-0 p-0 form-ellipsis " id="bi">行動裝置程式設計</p>
+				<p class="m-0 p-0 form-ellipsis " id="bi"> 
+
+					<?php
+						echo "hi";
+					?>
+				</p>
 			</div>
 
 			<div class="col border f-height centerVertically px-1 xuan-table" id="d3-2">
